8ce7d51efb65c89e47af8289251bcd45acd5cc67
Benjamin Reis <yasmindosireis@gmail.com>
2026-08-10 10:13:33 -0300

    Update TelemetrySubsystem.h

--- Telemetry/TelemetrySubsystem.h
+++ Telemetry/TelemetrySubsystem.h
@@ -2,52 +2,30 @@
 
 #include "CoreMinimal.h"
 #include "Subsystems/WorldSubsystem.h"
-
-#include "../Core/RobotTelemetry.h"
-
+#include "Core/RobotTelemetry.h"
 #include "TelemetrySubsystem.generated.h"
 
-class ARobotBase;
-
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(
-    FOnRobotTelemetryUpdated,
-    FRobotTelemetry,
-    Telemetry
-);
-
 UCLASS()
-class ROBOTSIMLAB_API UTelemetrySubsystem
-    : public UWorldSubsystem
+class ROBOTSIMLAB_API UTelemetrySubsystem : public UWorldSubsystem
 {
     GENERATED_BODY()
 
 public:
 
-    UFUNCTION(BlueprintCallable)
-    void RegisterRobot(ARobotBase* Robot);
+    UFUNCTION(BlueprintCallable, Category = "Telemetry")
+    void UpdateTelemetry(const FRobotTelemetry& NewTelemetry);
 
-    UFUNCTION(BlueprintCallable)
-    void UnregisterRobot(ARobotBase* Robot);
+    UFUNCTION(BlueprintPure, Category = "Telemetry")
+    FRobotTelemetry GetTelemetry() const;
 
-    UFUNCTION(BlueprintCallable)
-    FRobotTelemetry GetRobotTelemetry(
-        ARobotBase* Robot
-    ) const;
+    UFUNCTION(BlueprintPure, Category = "Telemetry")
+    bool HasWarning() const;
 
-    UPROPERTY(BlueprintAssignable)
-    FOnRobotTelemetryUpdated OnTelemetryUpdated;
-
-    virtual void Tick(
-        float DeltaTime
-    ) override;
-
-    virtual bool IsTickable() const override
-    {
-        return true;
-    }
+    UFUNCTION(BlueprintPure, Category = "Telemetry")
+    bool HasCriticalFault() const;
 
 private:
 
     UPROPERTY()
-    TArray<TObjectPtr<ARobotBase>> Robots;
+    FRobotTelemetry CurrentTelemetry;
 };
